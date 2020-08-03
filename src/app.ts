cb885d34b04d1936bd8ef640c7719356eaa91496
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-03 20:40:56 -0300

    add function  in appTs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express,{Application , Request, Response,NextFunction} from 'express';
 
 const app : Application = express();
+const add = (a:number, b:number): number => a + b ;
 
 app.get('/',(req:Request, res:Response, next:NextFunction)=>{
     res.send('hello world from typescript');
