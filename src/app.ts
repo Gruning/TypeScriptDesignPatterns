17e1441c5c9b80f7771cada435f7eff2a61c76a4
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-23 11:39:41 -0300

    sec3cap17_introducingInv

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-import express,{Application , Request, Response,NextFunction} from 'express';
 
-const app : Application = express();
-const add = (a:number, b:number): number => a + b ;
 
-app.get('/',(req:Request, res:Response, next:NextFunction)=>{
-    console.log(add(5,100));
+
+//-------------------------
+
+//import express,{Application , Request, Response,NextFunction} from 'express';
+
+//const app : Application = express();
+//const add = (a:number, b:number): number => a + b ;
+
+//app.get('/',(req:Request, res:Response, next:NextFunction)=>{
+//    console.log(add(5,100));
     
-    res.send('hello world from typescript');
-});
+//    res.send('hello world from typescript');
+//});
 
-app.listen(5000,()=>console.log('server running'))
+//app.listen(5000,()=>console.log('server running'))
+
+//---------------
 
 //TO RUN IN BROWSER 
 //npm run dev
